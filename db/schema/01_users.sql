32699352a11de5cd7f0e27bfeb339b8d5c868b21
anironL <viasolaris@gmail.com>
2022-12-16 15:02:25 -0500

    DB tables schema
    Split schema for imports to 01 users, 02 menu items, 03 orders

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,32 +1,6 @@
 DROP TABLE IF EXISTS users CASCADE;
-DROP TABLE IF EXISTS menuitems CASCADE;
-DROP TABLE IF EXISTS orders CASCADE;
-
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL
 )
-
-CREATE TABLE menu_items (
-  id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL,
-  price INTEGER NOT NULL  -- price in cents
-)
-
-CREATE TABLE orders (
-  id SERIAL PRIMARY KEY NOT NULL,
-  user_id INTEGER REFERENCES users ON DELETE CASCADE,
-  menu_item_id INTEGER REFERENCES menu_items ON DELETE CASCADE,
-  price INTEGER NOT NULL, -- price in cents, will be SUM(menu_items.price) in most/all cases
-  user_notes VARCHAR(255)
-  tip INTEGER,            -- in cents
-  tax INTEGER NOT NULL,   -- in cents
-  updated TIMESTAMP,      -- make it automatic?
-  created TIMESTAMP,
-  completed TIMESTAMP
-)
-
--- 1. users: id, name
--- 2.  menu: id,  name, price
--- 3. order: id, price, tip ( ? )
