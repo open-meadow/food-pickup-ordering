a8ab4550f0b3961d5bc6d50c88637cc0055bb1f9
anironL <viasolaris@gmail.com>
2022-12-16 18:15:44 -0500

    ERD update
    Orders & FKPK

--- db/schema/03_orders.sql
+++ db/schema/03_orders.sql
@@ -3,10 +3,8 @@ DROP TABLE IF EXISTS orders CASCADE;
 CREATE TABLE orders (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users ON DELETE CASCADE,
-  menu_item_id INTEGER REFERENCES menu_items ON DELETE CASCADE,
-  price INTEGER NOT NULL, -- price in cents, will be SUM(menu_items.price) in most/all cases
+  total_cost INTEGER NOT NULL, -- price in cents, will be SUM(menu_items.price) in most/all cases
   user_notes VARCHAR(255),
-  tip INTEGER,            -- in cents
   tax INTEGER NOT NULL,   -- in cents
   updated TIMESTAMP,      -- make it automatic?
   created TIMESTAMP,
