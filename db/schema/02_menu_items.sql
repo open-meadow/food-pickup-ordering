c80ba99ee4ccdcd055c140010a0944466dbf6858
anironL <viasolaris@gmail.com>
2022-12-16 17:24:37 -0500

    DB table seeds
    Update seeds users, menu_items, orders, orders_menu_items.

--- db/schema/02_menu_items.sql
+++ db/schema/02_menu_items.sql
@@ -3,5 +3,7 @@ DROP TABLE IF EXISTS menu_items CASCADE;
 CREATE TABLE menu_items (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
+  photo_url VARCHAR(255),
+  desc VARCHAR(255),
   price INTEGER NOT NULL  -- price in cents
 )
---
+++ db/schema/04_orders_menu_items.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS orders_menu_items.sql CASCADE;
+
+CREATE TABLE orders_menu_items (
+  id SERIAL PRIMARY KEY NOT NULL, 
+  order_id  INT NOT NULL REFERENCES orders ON DELETE CASCADE,
+  menu_item_id INT NOT NULL REFERENCES menu_items ON DELETE CASCADE
+)
