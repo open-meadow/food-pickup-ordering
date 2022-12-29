9ff5746d5b5d19322d3dbc518f99508e51f4bbb2
open-meadow <rohan.varughese@protonmail.com>
2022-12-29 13:32:41 -0500

    Add HTML skeleton

--- db/schema/04_orders_menu_items.sql
+++ db/schema/04_orders_menu_items.sql
@@ -1,7 +1,8 @@
 DROP TABLE IF EXISTS orders_menu_items.sql CASCADE;
 
 CREATE TABLE orders_menu_items (
-  id SERIAL PRIMARY KEY NOT NULL, 
+  id SERIAL PRIMARY KEY NOT NULL,
   order_id  INT NOT NULL REFERENCES orders ON DELETE CASCADE,
-  menu_item_id INT NOT NULL REFERENCES menu_items ON DELETE CASCADE
+  menu_item_id INT NOT NULL REFERENCES menu_items ON DELETE CASCADE,
+  quantity INT NOT NULL
 )
