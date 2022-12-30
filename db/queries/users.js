37fabf8aa7e7853a582dee4e986b11541d7266d6
Samuel Giorgievski <samgiorgievski@gmail.com>
2022-12-30 14:33:42 -0500

    add more functions

--- db/queries/users.js
+++ db/queries/users.js
@@ -34,7 +34,7 @@ const getOrderInfo = (email) => {
     JOIN orders ON users.usersID = orders.id
     JOIN orders_menu_items ON orders.id = orders_menu_items.order_id
     JOIN menu_items ON orders_menu_items.menu_item_id = menu_items.id
-    WHERE email = $1
+    WHERE email = $1;
     `,
     [`${email}`]
   )
@@ -47,13 +47,22 @@ const getOrderInfo = (email) => {
   });
 }
 
+// Function 3: fetch all menu items, use to populate menu on load
+const getMenuItems = () => {
+  return db.query(
+    `SELECT *
+    FROM menu_items`
+  )
+  .then((result) => {
+    return result.rows;
+  })
+};
 
-
-// Function 3: Login/registration - Check if user email exists (getUserByEmail)
+// Function 4: Login/registration - Check if user email exists (getUserByEmail)
 // Use getuserbyemail (revisit if needed)
 
 
-// Function 4: Check if user is logged in (cookies) (encryption)
+// Function 5: Check if user is logged in (cookies) (encryption)
 // revisit when we implement cookies
 
 const verifyCookie = () => {
@@ -64,7 +73,7 @@ const verifyCookie = () => {
 }
 
 
-// Function 5: Compare hashed passwords (hash)
+// Function 6: Compare hashed passwords (hash)
 // revisit when we implement password hash
 
 const verifyPassword = () => {
@@ -72,12 +81,100 @@ const verifyPassword = () => {
 
 }
 
-// Function 6: post to database
-const addUser
+// Function 7: post new user to database
+const addUser = () => {
+  return db.query (`
+    INSERT INTO users (name, email, password, phone)
+    VALUES ($1, $2, $3, $4)
+    RETURNING id;
+  `,
+  [`${users.name}`, `${users.email}`, `${users.password}`, `${users.phone}`])
+  .then((result) => {
+    return result.rows[0] //returns ID
+  })
+  .catch((error) => {
+    console.log(error.message)
+  })
+};
 
-  return db.
+// Function 8: update user name in database
+
+const updateUserName = (name, email) => {
+  return db.query (`
+    UPDATE users
+    SET users.name = $1
+    WHERE user.email = $2;
+    `,
+    [`${name}`, `${email}`]
+  )
+};
+
+// Function 9: update user phone in database
+
+const updateUserPhone = (phone, email) => {
+  return db.query (`
+    UPDATE users
+    SET users.phone = $1
+    WHERE user.email = $2;
+    `,
+    [`${phone}`, `${email}`]
+  )
+};
+
+// Function 10: update user password in database
+// Will utilize function 5
+
+const updateUserPassword = (currentPassword, newPassword, email) => {
+
+  const userInfo = getUserInfo(email);
+  // bcrypt not yet required
+  // if (bcrypt.compareSync(currentPassword, userInfo.password)) {
+  //   console.log ("ok it works")
+  // };
+
+  return db.query (`
+    UPDATE users
+    SET users.password = $1
+    WHERE user.email = $2;
+    `,
+    [`${newPassword}`, `${email}`]
+  )
+
+  // if bycrypt doesn't match return null
+};
 
 
+// Function 11: create order object for current order array
+
+// {
+//   cartItems: [
+//     object1 =
+//       {orderItem,
+//       quantity,
+//       price},
+//     object2 =
+//       {orderItem,
+//       quantity,
+//       price}
+//     ],
+//   // function to add cartItems
+//     function addCartItems = () => {
+
+//     },
+//   // function calculates Fees
+//     // iterate through cartitem array
+//     // add all prices
+//     // *.1
+//   // function calculates tax
+//   // function calculates total cost
+// };
+
+
+// Function 12: post order object (function 10)
+// Need to figure out how we work with current order object before we code this
+
+
+//FUNCTION 13: logout?
 
 // NON DATABASE FUNCTIONS
 // Function: Create right pane element??? may be a script
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,11 +1,102 @@
 // Client facing scripts here
 //FUNCTIONS
+//  MENU ITEM GENERATION
+const createMenuItem = function(){
 
+}
+const renderMenuItem = function(){
 
+}
+//  CART GENERATION/MODIFICATION
+const createCartItem = function () {
+  // from the menu we take the price and menuitemID then increment up quantity by 1
+  // create an item like :
+  // object1 =
+    // {orderItem,
+    // quantity,
+    // price}
+  // return object1
+}
+const addCartItem = function () {
+  // check the cart to see if duplicate name/price if yes +1 quantity else new object
+}
+const removeCartItem = function () {
+  // reduce quantity of specified object by 1; if 0, "delete" object
+}
+
+const calculateFees = function () {
+
+};
+const calculateTaxes = function () {
+
+};
+const calculateTotalCost = function () {
+
+};
+  // function calculates Fees
+    // iterate through cartitem array
+    // add all prices
+    // *.1
+  // function calculates tax
+  // function calculates total cost
+
+const confirmOrder = () => {
+  // take cart items from addCartItem
+  // take total cost
+  // export to database
+}
 
 //SCRIPTS
-
 $(document).ready(function() {
-  console.log("hi friend");
+  console.log("website is loaded ok");
 
+  // move currentOrderCart init to login success
+  let currentOrderCart = [];
+  console.log("cart initialized", currentOrderCart);
+
+//MENU SCRIPTS
+  // $(document) ready
+  // generate the menu item objects and render
+
+// HEADER SCRIPT
+  // $(my_account).onclick
+    // animate left pane appearing
+  // $(cart).onclick
+    // animate right pane appearing
+
+//LEFT PANE SCRIPTS
+  // $(edit information button) onclick
+    // create field to update users.name/phone/password
+    // $(submit edit?) onclick set change to DB
+  // current order
+    // has a timer once $(submit order) onclick confirmed?
+    // start after API usage
+
+//RIGHT PANE SCRIPTS
+  // $(add order button from menu/right-pane) onclick add item
+    // createCartItem()
+    // addCartItem()
+  // $(add order button from menu/right-pane) onclick calculate fees, taxes, total
+    // calculateFees()
+    // calculateTaxes()
+    // calculateTotalCost()
+    // reload right pane items
+
+  // $(remove order button from right-pane) onclick calculate fees, taxes, total
+    // remove right pane item
+    // calculateFees()
+    // calculateTaxes()
+    // calculateTotalCost()
+    // reload right pane items
+
+  // $(submit order) onclick push all to DB
+    // send fees, taxes, totalcost to orders table
+    // send each object with attached usersID to orders_menu_items table
+
+  // $(cancel order) onlcikc delete all objects in currentOrderCart array
+    // remove right pane items
+    // calculateFees()
+    // calculateTaxes()
+    // calculateTotalCost()
+    // reload right pane items
 });
