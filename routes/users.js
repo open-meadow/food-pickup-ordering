0011c109be7cf91da563be88c8f59579b1d9b899
anironL <viasolaris@gmail.com>
2023-01-02 16:07:02 -0500

    Add DB requirement to routing
    Users routes require DB as argument, will call automatically instead of having to declare every time.

--- routes/users.js
+++ routes/users.js
@@ -7,9 +7,31 @@
 
 const express = require('express');
 const router  = express.Router();
+module.exports = (db) => {
+  router.get('/', (req, res) => {
+    res.render('users');
+  });
 
-router.get('/', (req, res) => {
-  res.render('users');
-});
+  // route to create menu items /users/createMenu
+  router.get('/createMenu', (req, res) => {
+    return db
+    .query(
+      `SELECT *
+      FROM menu_items`
+      )
+    .then ((result) => {
+      return res.json(result.rows); 
+    });
+  });
 
-module.exports = router;
+  // 
+  router.post('/completeOrder', (req, res) => {
+    return db
+    .query(
+      `INSERT INTO `
+    )
+    console.log("complete order then push to DB")
+  });
+
+  return router;
+  }
