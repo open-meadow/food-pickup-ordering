264013dfcc313c1cca6ffdfe2b4ef04a1974ce20
Samuel Giorgievski <samgiorgievski@gmail.com>
2023-01-02 12:47:17 -0500

    style and organize header with scss

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const sassMiddleware = require('./lib/sass-middleware');
 const express = require('express');
 const morgan = require('morgan');
 
+
 const PORT = process.env.PORT || 8080;
 const app = express();
 
