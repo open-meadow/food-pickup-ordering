98dc58a6fac3ceb35862eeacaab2d607a5e33dbd
anironL <viasolaris@gmail.com>
2023-01-03 17:00:03 -0500

    Right pane functionality
    Implement right pane functionality for refreshing order/fees/total costs & cancel order

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -12,59 +12,105 @@ const renderMenuItems = function(menuItems) {
 };
 const createMenuItem = function(menuItem) {
   let $menuItem = (
-    `<div class = "menu_item">
+    `<div id = "${menuItem.id}" class = "menu_item">
       <img src = "${menuItem.photo_url}"></img>
       <section class = "menu_text">
         <span><b>${menuItem.name}</b></span>
         <span>$${(menuItem.price)/100}</span>
         <p>${menuItem.descrip}</p>
       </section>
-      <button type = "submit">(+)</button>
-      <button type = "submit">(-)</button>
+      <button type="button" onclick="incrementClick(${menuItem.id})">(+)</button>
+      <button type="button" onclick="decrementClick(${menuItem.id})">(-)</button>
     </div>`);
+  allMenuItems.push({
+    id: menuItem.id,
+    name: menuItem.name,
+    photo_url: menuItem.photo_url,
+    descrip: menuItem.descrip,
+    price: menuItem.price,
+  })
+  console.log(allMenuItems)
   return $menuItem;
-}
-
+};
 //  CART GENERATION/MODIFICATION
-const createCartItem = function () {
-  // from the menu we take the price and menuitemID then increment up quantity by 1
-  // create an item like :
-  // object1 =
-    // {orderItem,
-    // quantity,
-    // price}
-  // return object1
-}
-const addCartItem = function () {
-  // check the cart to see if duplicate name/price if yes +1 quantity else new object
-}
-const removeCartItem = function () {
-  // reduce quantity of specified object by 1; if 0, "delete" object
-}
+// Increment sesion storage per menu item by ID
 
-const calculateFees = function () {
-
+function incrementClick(idNum) {
+  if (typeof(Storage) !== "undefined") {
+    if (localStorage.getItem(idNum)) {
+      let attempts = parseInt(localStorage.getItem(idNum));
+      // console.log(`storage value for key, ${idNum} = ${attempts}`);
+      localStorage.setItem(`${idNum}`, ++attempts);
+    } else {
+      localStorage.setItem(`${idNum}`, 1);
+    }
+    renderCurrentOrderPane();
+    // console.log("localstorage", localStorage)
+  }
 };
-const calculateTaxes = function () {
-
+// Decrement session storage per menu item by ID
+function decrementClick(idNum) {
+  if (typeof(Storage) !== "undefined") {
+    if (localStorage[idNum] > 0) {
+      localStorage[idNum] = Number(localStorage[idNum]) - 1;
+      if (Number(localStorage[idNum]) === 0) {
+        localStorage.removeItem(idNum)
+      }
+    } 
+    renderCurrentOrderPane();
+  } 
 };
-const calculateTotalCost = function () {
+function renderCurrentOrderPane() {
+  document.getElementById("cart_items").innerHTML = "";
+  document.getElementById("fees_box").innerHTML = "";
 
+  let netOrder = 0;
+  let fees = 300;
+  let taxes = 0;
+  let totalOrder = 0;
+
+  if (typeof(Storage) !== "undefined") {
+    for (key in localStorage) {
+      // push keys to array, sort and return by sorted
+      if (typeof localStorage[key] !== "function" && key !== "length") {
+        document.getElementById("cart_items").innerHTML += `<li>${localStorage[key]} times x ${allMenuItems[key-1].name} name $${allMenuItems[key-1].price * localStorage[key] / 100} price</li>`
+        
+        // moMoneyCalculator (move out calculations)
+        netOrder = netOrder + allMenuItems[key-1].price * localStorage[key];
+        taxes = Math.round(netOrder * 0.13);
+        totalOrder = netOrder + fees + taxes;
+      }
+    }
+    document.getElementById("fees_box").innerHTML += `
+      <p>  $${(netOrder/100).toFixed(2)} net</p>  
+      <p>+ $${(fees/100).toFixed(2)} fees</p>  
+      <p>+ $${(taxes/100).toFixed(2)} taxes</p> 
+      <p> ----- </p> 
+      <p>$${(totalOrder/100).toFixed(2)}</p>`;
+  }
+  currentTotals.fee = fees;
+  currentTotals.tax = taxes;
+  currentTotals.total = totalOrder;
+  console.log(currentTotals);
 };
-  // function calculates Fees
-    // iterate through cartitem array
-    // add all prices
-    // *.1
-  // function calculates tax
-  // function calculates total cost
 
 const confirmOrder = () => {
+  // confirm name & phone #
+}
+
+const completeOrder = () => {
   // take cart items from addCartItem
   // take total cost
   // export to database
+  localStorage.clear();
 }
+const cancelOrder = () => {
+  localStorage.clear();
+  renderCurrentOrderPane();
+}; 
 
 //SCRIPTS
+// onload
 $(document).ready(function() {
   console.log("website is loaded ok");
   $.get("/users/createMenu")
@@ -72,39 +118,30 @@ $(document).ready(function() {
       renderMenuItems(response);
       // console.log(response);
     });
+// currentOrderCart init on load
+  currentOrderCart = {};
+  console.log("cart initialized", typeof(currentOrderCart));
+  allMenuItems = [];
+  console.log("menu cart initialized");
+  currentTotals = {
+    fee: 0,
+    tax: 0,
+    total: 0
+  };
+  $("confirm").click(function(){
+    completeOrder();
+    // $.post("/users/completeOrder")
+  });
+});
 
-  // move currentOrderCart init to login success
-  let currentOrderCart = [];
-  console.log("cart initialized", currentOrderCart);
-
-  //MENU SCRIPTS
-  // $(document) ready
-  // generate the menu item objects and render
-  renderMenuItems(menuItems);
-
-// HEADER SCRIPT
+// HEADER SCRIPTS TO BE IMPLEMENTED
   // $(cart).onclick
     // animate right pane appearing
   // $(".cart_icon").click(function() {
   //   $(".right_pane pure-u-1-5").toggle(500);
   // });
 
-//RIGHT PANE SCRIPTS
-  // $(remove order button from right-pane) onclick calculate fees, taxes, total
-    // remove right pane item
-    // calculateFees()
-    // calculateTaxes()
-    // calculateTotalCost()
-    // reload right pane items
-
+//RIGHT PANE SCRIPTS TO BE IMPLEMENTED
   // $(submit order) onclick push all to DB
     // send fees, taxes, totalcost to orders table
     // send each object with attached usersID to orders_menu_items table
-
-  // $(cancel order) onlcikc delete all objects in currentOrderCart array
-    // remove right pane items
-    // calculateFees()
-    // calculateTaxes()
-    // calculateTotalCost()
-    // reload right pane items
-});
--- routes/users.js
+++ routes/users.js
@@ -24,14 +24,47 @@ module.exports = (db) => {
     })
   });
 
-  //
-  router.post('/completeOrder', (req, res) => {
-    return db
-    .query(
-      `INSERT INTO `
-    )
-    console.log("complete order then push to DB")
-  });
+ 
+  // router.post('/completePersonalInfo', (req, res) => {
+  //   return db
+  //   .query(
+  //     `INSERT INTO USERS (name, phone)
+  //     VALUES ($1, $2)
+  //     RETURNING ID
+  //     `,
+  //     []
+  //   )
+  //   .then((result) => {
+  //     console.log("ID pushed to DB")
+  //   })
+  // });
+
+  // router.post('/completeOrder', (currentCart) => {
+  //   return db
+  //   .query(
+  //     `INSERT INTO ORDERS (total_cost, fees, tax, created)
+  //     VALUES ($1, $2, $3, $4)
+  //     RETURNING ID
+  //     `,
+  //     []
+  //   )
+  //   .then((result) => {
+  //     console.log("complete order then push to DB");
+  //     orderMenuItems(res);
+  //   })
+  // });
+
+  // const orderMenuItems = (orderID) => {
+  //   return db
+  //   .query(
+  //     `INSERT INTO ORDERS_MENU_ITEMS (order_id, menu_item_id, quantity)
+  //     VALUES ($1, $2, $3)
+  //     `,
+  //     [orderID]
+  //   )
+  //   console.log("complete order then push to DB")
+  // };
+
 
   return router;
   }
