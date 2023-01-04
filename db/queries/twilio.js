e56a198995785cfbe61b1c67b8ea126c6aff4663
anironL <viasolaris@gmail.com>
2023-01-04 11:36:43 -0500

    Twilio & package
    Twilio setup

--- db/queries/twilio.js
+++ db/queries/twilio.js
@@ -4,14 +4,24 @@ const db = require('../connection');
 // Find your Account SID and Auth Token at twilio.com/console
 // and set the environment variables. See http://twil.io/secure
 const accountSid = ACed0f4de7ac43019039992fa77cda9658;
+<<<<<<< Updated upstream
 const authToken = '83ff14a9bab35cdd25d040c6675f225c';
+=======
+const authToken = `83ff14a9bab35cdd25d040c6675f225c`;
+>>>>>>> Stashed changes
 const client = require('twilio')(accountSid, authToken);
 
 client.messages
   .create({
+<<<<<<< Updated upstream
     to: '+6473776171',
     from: '+19897955570',
     body: 'This is the ship that made the Kessel Run in fourteen parsecs?'
+=======
+     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+     from: '+19897955570',
+     to: '+16473776171'
+>>>>>>> Stashed changes
    })
   .then(message => console.log(message.sid));
 
