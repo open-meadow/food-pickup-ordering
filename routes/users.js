373cf796e4c8c7a1257fae3872c64bf36a7054fb
anironL <viasolaris@gmail.com>
2023-01-05 13:20:05 -0500

    Refactor order complete function
    Refactor so that the route does not contain several functions

--- routes/users.js
+++ routes/users.js
@@ -5,166 +5,136 @@
  */
 
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const { sendClientText, sendRestoText } = require("../db/queries/twilio.js")
+
 module.exports = (db) => {
   router.get('/', (req, res) => {
     res.render('users');
   });
 
-const { sendClientText, sendRestoText } = require("../db/queries/twilio.js")
-
   // route to create menu items /users/createMenu
   router.get('/createMenu', (req, res) => {
     return db
-    .query(
-      `SELECT *
+      .query(
+        `SELECT *
       FROM menu_items`
       )
-    .then((result) => {
-      return res.json(result.rows);
-    })
+      .then((result) => {
+        return res.json(result.rows);
+      })
+  });
 
-
-
-
-    });
-
-  // Currently testing
-  router.post('/complete_order', (req, res) => {
-    console.log(req.body);
-
-    let name = req.body.name;
-    let phone = req.body.phone;
-    let cart = req.body.cart;
-    let totalCost = req.body.totals["totalCost"];
-    let fees = req.body.totals["fees"];
-    let taxes = req.body.totals["taxes"];
-    let menuItemId;
-    let quantity = 0;
-
-    console.log("totalCost", totalCost);
-
-    console.log("Cart", cart)
-    let lateQuery = ``;
-
-
+  // Complete order, insert to DB the following: users, orders, order menu items
+  router.post('/complete_order', (req, res) => {  
     return db
     .query(`
     INSERT INTO USERS (name, phone)
     VALUES ($1, $2)
     RETURNING ID
     `,
-    [`${req.body.name}`, `${req.body.phone}`]
-    )
-    .then ((result) => {
-
-
-
-      console.log("result.rows", result.rows[0]);
-      const userID = result.rows[0].id;
-
-      console.log("userID", userID);
-
-      // initialize values to send to database
-      const created = new Date();
-      console.log("created", created.toString());
-      const required_time = created;
-      const completed = false;
-
-      const queryParams = [userID, totalCost, fees, taxes, created, required_time, completed];
-
-      return db
-      .query(`
-      INSERT INTO orders (user_id, total_cost, fees, tax, created, required_time, completed)
-      VALUES ($1, $2, $3, $4, $5, $6, $7)
-      RETURNING id;
-      `,
-      queryParams)
-      .then((result) => {
-        const orderID = result.rows[0].id;
-        lateQuery = `INSERT INTO orders_menu_items (order_id, menu_item_id, quantity) VALUES `
-        for (let menuitem in cart) {
-          menuItemId = menuitem.split('_')[2];
-          quantity = cart[menuitem];
-
-          lateQuery += `(${orderID}, ${menuItemId}, ${quantity}),`
-        }
-        lateQuery = lateQuery.slice(0, lateQuery.length - 1);
-        lateQuery += `;`
-
-        console.log("late query", lateQuery);
-        // const queryParams2 = [orderID, menuItemId, quantity];
-        return db
-        .query(lateQuery)
-        .then((result) => {
-          console.log("Successsssssss!!!!!!")
-          sendRestoText(orderID);
-        })
-        .catch((err) => {
-          console.log("who cares", err);
-        })
+    [`${req.body.name}`, `${req.body.phone}`])
+    .then((result) => {
+      insertOrders(req, result)
+      .then((result) => { 
+        insertOrdersMenuItems(req, result);
       });
     });
   });
-
-
-
-
-
-
-
+// DB query to insert order to orders
+  const insertOrders = function (req, result) {
+    const created = new Date();
+    const required_time = created;
+    const queryParams = [
+      result.rows[0].id, 
+      req.body.totals["totalCost"], 
+      req.body.totals["fees"], 
+      req.body.totals["taxes"], 
+      created, 
+      required_time, 
+      false
+    ];
+    
+    return db
+    .query(`
+    INSERT INTO orders (user_id, total_cost, fees, tax, created, required_time, completed)
+    VALUES ($1, $2, $3, $4, $5, $6, $7)
+    RETURNING id;
+    `, queryParams)
+  };
+// DB query to insert all menu items for a order to orders_menu_items
+  const insertOrdersMenuItems = function(req, result) {
+    let orderID = result.rows[0].id;
+    let cart = req.body.cart;
+    let menuItemId;
+    let quantity = 0;
+    let lateQuery = `INSERT INTO orders_menu_items (order_id, menu_item_id, quantity) VALUES `
+    // Create the query string
+    for (let menuitem in cart) {
+      menuItemId = menuitem.split('_')[2];
+      quantity = cart[menuitem];
+      lateQuery += `(${orderID}, ${menuItemId}, ${quantity}),`
+    }
+    lateQuery = lateQuery.slice(0, lateQuery.length - 1);
+    lateQuery += `;`
+    // Insert query string into DB orders_menu_items table and send SMS
+    return db
+    .query(lateQuery)
+      .then((result) => {
+        sendRestoText(orderID);
+      })
+      .catch((err) => {
+        console.log("DB write error:", err);
+      })
+  };
+// Generate all orders information
   router.get('/generateOrders', (req, res) => {
     return db
-    .query(`SELECT * FROM orders;`)
-    .then((result) => {
-      const orders = result.rows;
+      .query(`SELECT * FROM orders;`)
+      .then((result) => {
+        const orders = result.rows;
 
-      return db.query(`SELECT *
+        return db.query(`SELECT *
       FROM orders_menu_items
       JOIN menu_items ON orders_menu_items.menu_item_id = menu_items.id;`)
-      .then((result2) => {
-        orders.forEach(order => {
-          order.items = getItems(order.id, result2.rows);
-        });
-
-        return res.json(orders);
+          .then((result2) => {
+            orders.forEach(order => {
+              order.items = getItems(order.id, result2.rows);
+            });
+            return res.json(orders);
+          })
       })
-    })
   });
 
   router.post('/addTime', (req, res) => {
-    // increase required_time
+    // increase timer required_time
     const order_id = req.body["order_id"];
-
     let additionalTime = req.body["extra-time"];
     additionalTime = additionalTime * 60000;
 
     return db
-    .query(`
+      .query(`
       SELECT * FROM orders
       JOIN users ON orders.user_id = users.id
       WHERE orders.id = $1;
-    `,
-      [order_id])
+    `,[order_id])
     .then((result) => {
       // calculate new time
       const reqTime = new Date().getTime();
       const newTime = new Date(reqTime + additionalTime);
-
+      const queryParams = [newTime, order_id]
       let phoneNumber = Number(result.rows[0].phone);
       // console.log("result.rows", result.rows[0].phone);
 
-      const queryParams = [ newTime, order_id ]
-
       return db
       .query(`UPDATE orders
-              SET required_time = $1
-              WHERE id = $2
-              RETURNING *;`, queryParams)
-        .then((result) => {
-          // sendClientText(`${phoneNumber}`, `${req.body["extra-time"]}`);
-          return res.redirect('/restaurant');
-        })
+          SET required_time = $1
+          WHERE id = $2
+          RETURNING *;`, queryParams)
+      .then((result) => {
+        // sendClientText(`${phoneNumber}`, `${req.body["extra-time"]}`);
+        return res.redirect('/restaurant');
+      })
     })
     .catch((err) => {
       console.log("no required-time yet")
@@ -178,10 +148,10 @@ const { sendClientText, sendRestoText } = require("../db/queries/twilio.js")
 
     return db
     .query(`
-        UPDATE orders
-        SET completed = $1
-        WHERE id = $2
-        RETURNING *;
+      UPDATE orders
+      SET completed = $1
+      WHERE id = $2
+      RETURNING *;
     `, [true, order_id])
     .then((result) => {
       return res.redirect('/restaurant');
@@ -198,16 +168,6 @@ const { sendClientText, sendRestoText } = require("../db/queries/twilio.js")
     return result;
   }
 
-  // const orderMenuItems = (orderID) => {
-  //   return db
-  //   .query(
-  //     `INSERT INTO ORDERS_MENU_ITEMS (order_id, menu_item_id, quantity)
-  //     VALUES ($1, $2, $3)
-  //     `,
-  //     [orderID]
-  //   )
-  //   console.log("complete order then push to DB")
-  // };
 
   return router;
-  }
+};
